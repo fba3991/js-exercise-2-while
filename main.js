4ca098d1fbe6dffaf917b44c6e1d9fa9d2501cb0
Fabioes <fabionike@icloud.com>
2023-10-24 17:13:41 +0200

    feat / esercizi

---
+++ main.js
@@ -0,0 +1,43 @@
+/* let num = prompt('inserisci un numero da 1 a 10');
+let i  = 1;
+while( i <=10 ) {
+ alert( num + 'x' + i + '=' + (num * i ));
+ i++ ;
+} 
+  */
+/* let frase = prompt('scrivi una frase');
+let parola1 = prompt('inscerisci parola da sostituire');
+let parola2 = prompt('inscerisci sostituo');
+frase = frase.replace  (parola1, parola2);
+alert(frase); */
+
+/* let url = prompt('inserisci un url');
+if((url.startsWith ('http://' || 'https://')) && url.endsWith('.com')){
+    alert('url valido');
+} else{
+    alert('url non valido');
+} */
+
+let vittoriePc = 0;
+let vittorieG = 0;
+let gioco = 'si';
+alert('lancia il dado');
+while(gioco == 'si'){
+
+  let dadoPc = Math.ceil (Math.random() * 6);
+  let dadoG = Math.ceil (Math.random() * 6);
+  alert(' giocatore: '  + dadoG + ' ' + ' pc: ' +dadoPc);
+  if(dadoG > dadoPc){
+    alert('hai vinto');
+    vittorieG++;
+}   
+else if  ( dadoG < dadoPc){
+alert('hai perso');
+vittoriePc++;
+} 
+else{
+  alert('pareggio')
+};
+alert('punteggio: ' + )
+ 
+}
